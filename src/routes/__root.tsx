82967c537a1d8e5a7061d546b5197f3585045dfd
ed-barnes937 <ed.barnes937@gmail.com>
2025-09-22 15:34:06 +0100

    allow dev tools in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,40 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-import { type RootContext } from '@/routerContext'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { type RootContext } from "@/routerContext";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { lazy, Suspense, useEffect, useState } from "react";
 
 export const Route = createRootRouteWithContext<RootContext>()({
-  component: App
-})
+  component: App,
+});
+
+const ReactQueryDevtoolsProduction = lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+);
 
 function App() {
-  return (<div className='w-screen h-screen flex flex-col'>
+  const [showDevtools, setShowDevtools] = useState(false);
+
+  useEffect(() => {
+    // @ts-expect-error
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
+  return (
+    <div className="w-screen h-screen flex flex-col">
       <Outlet />
-      <TanStackRouterDevtools position='bottom-right' />
-      <ReactQueryDevtools buttonPosition='bottom-left' position='bottom' />
+      <TanStackRouterDevtools position="bottom-right" />
+      <ReactQueryDevtools buttonPosition="bottom-left" position="bottom" />
+      {showDevtools && (
+        <Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </Suspense>
+      )}
     </div>
-  )
+  );
 }
